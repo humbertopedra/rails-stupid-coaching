e25e6a667811340978226b3d935cb35137554b60
Humberto Pedra <hphumbertopedra@yahoo.com.br>
2023-08-21 15:06:35 -0300

    stupid coaching, mas não fiz o opcional ainda

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @response = params[:question]
+    if @response == 'I am going to work'
+      @reply = 'Great!'
+    elsif @response.include?('?')
+      @reply = 'Silly question, get dressed and go to work!'
+    else
+      @reply = "I don´t care, get dressed and go to work!"
+    end
+  end
+end
